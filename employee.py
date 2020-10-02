59af6dd5d5e4f997c3aa6802474ef1519b8b2602
Noa Dudai <nooninana@gmail.com>
2020-10-02 20:55:22 +0300

    fixed the add_employee_manually

--- employee.py
+++ employee.py
@@ -9,16 +9,20 @@ class Employee:
         self.age = age
 
 
+with open('Employees.csv', 'w', newline='') as csv_employees_file:
+    fieldnames = ['id', 'name', 'phone', 'age']
+    writer = csv.DictWriter(csv_employees_file, fieldnames = fieldnames)
+    writer.writeheader()
+
+
 employee1 = Employee(1, "Jeane Dee", 972598386743, 23)
 
 
 def add_employee_manually(self):
     with open('Employees.csv', 'a', newline='') as csvfile:
         fieldnames = ['id', 'name', 'phone', 'age']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-        writer.writeheader()
-        writer.writerow({'id': self.id, 'name': self.name, 'phone': self.phone, 'age': self.age})
+        my_writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+        my_writer.writerow({'id': self.id, 'name': self.name, 'phone': self.phone, 'age': self.age})
 
 
 add_employee_manually(employee1)
