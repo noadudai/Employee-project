8f97c39937488b16ec606b2f0326b8d486977dd6
Noa Dudai <nooninana@gmail.com>
2020-10-07 22:41:48 +0300

    deleted files and code to re start.

--- employee.py
+++ employee.py
@@ -1,5 +1,3 @@
-import csv
-
 
 class Employee:
     def __init__(self, id, name, phone, age):
@@ -8,21 +6,3 @@ class Employee:
         self.phone = phone
         self.age = age
 
-
-with open('Employees.csv', 'w', newline='') as csv_employees_file:
-    fieldnames = ['id', 'name', 'phone', 'age']
-    writer = csv.DictWriter(csv_employees_file, fieldnames = fieldnames)
-    writer.writeheader()
-
-
-employee1 = Employee(1, "Jeane Dee", 972598386743, 23)
-
-
-def add_employee_manually(self):
-    with open('Employees.csv', 'a', newline='') as csvfile:
-        fieldnames = ['id', 'name', 'phone', 'age']
-        my_writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-        my_writer.writerow({'id': self.id, 'name': self.name, 'phone': self.phone, 'age': self.age})
-
-
-add_employee_manually(employee1)
