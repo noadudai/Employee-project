914263f2847fad6222b6e04c68033a4a1212e9ca
Noa Dudai <nooninana@gmail.com>
2020-10-09 15:54:25 +0300

    adjustment

--- employee.py
+++ employee.py
@@ -9,18 +9,8 @@ class Employee:
         self.age = age
 
 
-def dict_employee(self):
-    employee = {"id": self.id, "name": self.name, "phone": self.phone, "age": self.age}
-    return employee
+employee1 = Employee(1, 'Jane Doe', 972568792519, 23),
+employee2 = Employee(2, 'Lily Pow', 972568925410, 24),
+employee3 = Employee(3, 'Dean Sam', 972541908673, 23),
 
 
-employee1 = Employee(1, 'Jane Doe', 972568792519, 23)
-
-
-def add_employee_manually_to_epmloyees_file(self):
-    with open('Employees.jason', 'a') as json_file:
-        x = dict_employee(self)
-        json_file.write(json.dumps(x))
-
-
-add_employee_manually_to_epmloyees_file(employee1)
