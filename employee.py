0caf2d002b392bc828c2c971f1e9bf09e7dcbf04
Noa Dudai <nooninana@gmail.com>
2020-10-09 18:34:32 +0300

    Created a function named add_employee_manually_to_employees_file

--- employee.py
+++ employee.py
@@ -9,4 +9,20 @@ class Employee:
         self.age = age
 
 
+employee1 = Employee(1, 'Jane Doe', 972567824516, 23)
+employee2 = Employee(2, 'Luke Main', 972568930980, 25)
 
+
+def dict_employee(self):
+    employee = {'id': self.id, 'name': self.name, 'phone': self.phone, 'age': self.age}
+    return employee
+
+
+def add_employee_manually_to_epmloyees_file(emp):
+    x = dict_employee(emp)
+    with open('Employees.json', 'a') as json_file:
+        json_file.write(json.dumps(x) + ', \n')
+
+
+add_employee_manually_to_epmloyees_file(employee1)
+add_employee_manually_to_epmloyees_file(employee2)
