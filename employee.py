e5ce3b40ca8853001216fde69958908894d19044
Noa Dudai <nooninana@gmail.com>
2020-10-11 00:30:06 +0300

    Fixed Add_employee function

--- employee.py
+++ employee.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+file_path = 'Employees.json'
+
 
 class Employee:
     def __init__(self, id, name, phone, age):
@@ -18,11 +20,33 @@ employee1 = Employee(1, 'Jane Doe', 972567824516, 23)
 employee2 = Employee(2, 'Luke Main', 972568930980, 25)
 
 
-def add_employee_manually_to_epmloyees_file(emp):
-    x = emp.dict_employee()
+def load_employees():
+    if not os.path.exists(file_path):
+        return []
+
     with open('Employees.json', 'r') as json_file:
+        read_file = json_file.read()
+        if read_file == "":
+            return []
+        employees = json.loads(read_file)
+        return [Employee(employee["id"], employee["name"], employee["phone"], employee["age"]) for employee in
+                employees]
 
 
+load_employees()
 
-add_employee_manually_to_epmloyees_file(employee1)
-add_employee_manually_to_epmloyees_file(employee2)
+
+def save_employees(employees):
+    dict_employees = [employee.dict_employee() for employee in employees]
+    with open('Employees.json', 'w') as json_file:
+        json_file.write(json.dumps(dict_employees, indent=2))
+
+
+def add_employee_manually_to_employees_file(employee):
+    employees = load_employees()
+    employees.append(employee)
+    save_employees(employees)
+
+
+add_employee_manually_to_employees_file(employee1)
+add_employee_manually_to_employees_file(employee2)
